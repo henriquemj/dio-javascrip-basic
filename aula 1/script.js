09642f43ad155f9e50db8642df40e97e8eb4220d
Henrique Madureira <henrique.madureira@seuconsumo.com.br>
2021-11-26 11:17:22 -0300

    subindo as aulas

---
+++ aula 1/script.js
@@ -0,0 +1,4 @@
+/*
+alert("Desenvolvendo Alerta") */
+
+
---
+++ aula 2/script.js
@@ -0,0 +1,37 @@
+
+ // divisão real
+ var divisaoReal = 6 / 2;
+ console.log(divisaoReal);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// pontenciação
+var pontenciacao = 2 ** 10;
+console.log(pontenciacao);
+
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue); 
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a 
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);
+
+var e = true && true;
+console.log(e);
+
+var ou = false || false;
+console.log(ou) 
+
+var nao =!true;
+console.log(nao);
